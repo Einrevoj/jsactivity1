9dc0ab108e5a014b7b0952f8b39372fdec039864
Einrevoj <jovcano28@gmail.com>
2022-07-23 16:09:07 +0800

    rename

--- activity.js
+++ activity.js
@@ -1,7 +1,9 @@
 let result;
 let title = "What is your name?";
 let defaultValue = "input";
+let isYourName = confirm("Is that your name?")
 
 result = prompt(title, [defaultValue])
 alert(`Hello ${result}`)
+alert(isYourName)
 result = confirm(question)
