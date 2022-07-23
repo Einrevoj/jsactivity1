2575eda171718d9ae60c7cb030b06969efbb9675
Einrevoj <jovcano28@gmail.com>
2022-07-23 16:12:52 +0800

    fixed codes

--- activity.js
+++ activity.js
@@ -1,9 +1,12 @@
 let result;
 let title = "What is your name?";
 let defaultValue = "input";
-let isYourName = confirm("Is that your name?")
+
 
 result = prompt(title, [defaultValue])
 alert(`Hello ${result}`)
+
+let isYourName = confirm("Is that your name?")
 alert(isYourName)
 result = confirm(question)
+
